61a85f84c65a77a4ed8dc8bbc0f3e4050d29534e
MAEDA Go <maeda@farend.jp>
2011-09-11 14:08:41 +0900

    show count of overdue issues.

--- app/controllers/mobile_controller.rb
+++ app/controllers/mobile_controller.rb
@@ -4,5 +4,13 @@ class MobileController < ApplicationController
   before_filter :require_login
 
   def index
+    @overdue_issues = Issue.visible.open.find(:all,
+      :conditions => [
+        "(assigned_to_id = ? or assigned_to_id is null) and due_date < ?",
+        User.current.id,
+        Date.today,
+      ],
+      :include => [:status, :project, :tracker, :priority],
+      :order => "due_date")
   end
 end
