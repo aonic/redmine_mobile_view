2b6764b3dc1e21a234e6e98212d73ef57d8d7bfc
MAEDA Go <maeda@farend.jp>
2011-09-13 23:06:03 +0900

    list overdue issues' subject.

--- app/controllers/mobile_controller.rb
+++ app/controllers/mobile_controller.rb
@@ -4,6 +4,30 @@ class MobileController < ApplicationController
   before_filter :require_login
 
   def index
+    @overdue_issues = get_overdue_issues
+  end
+
+  def issues_list
+    case params[:list_mode]
+    when 'overdue'
+      @issues = get_overdue_issues
+    else
+      @issues = get_my_issues
+    end
+  end
+
+  private
+  def get_my_issues
+    @overdue_issues = Issue.visible.open.find(:all,
+      :conditions => [
+        "assigned_to_id = ?",
+        User.current.id,
+      ],
+      :include => [:status, :project, :tracker, :priority],
+      :order => "due_date")
+  end
+
+  def get_overdue_issues
     @overdue_issues = Issue.visible.open.find(:all,
       :conditions => [
         "(assigned_to_id = ? or assigned_to_id is null) and due_date < ?",
@@ -13,4 +37,5 @@ class MobileController < ApplicationController
       :include => [:status, :project, :tracker, :priority],
       :order => "due_date")
   end
+
 end
